116ad26df5dd310a5030a1c771ee87954582d910
alesandro-ucb <alesandro.santander@ucb.edu.bo>
2025-08-27 16:42:34 -0400

    utilizacion de double

---
+++ PRACTICA_01/Ejercicio_01_03.cpp
@@ -0,0 +1,18 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Alesandro Raul Santander Fernandez
+// Carnet: 13552698 L.P.
+// Carrera del estudiante: Ingeniería en Telecomunicaciones
+// Fecha creación: 27/08/2025
+// Número de ejercicio: 3
+#include <iostream>
+using namespace std;
+int main() {
+        double base ,altura, area; // double se coloca para entre varaibles con decimales y sea mas preciso el resultado, tambien se puede colocar int pero solo admite valores sin decimales 
+        cout << "Ingrese la base del triangulo: ";
+        cin >> base;
+        cout << "Ingrese la altura del triangulo: ";
+        cin >> altura;
+        area = (base * altura) / 2;
+        cout << "El area del triangulo es: " << area << endl;
+        return 0;
+}
