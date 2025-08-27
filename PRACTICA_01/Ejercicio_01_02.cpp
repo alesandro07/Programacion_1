fec6152bd1de6d1f9ec69050d0047bc034f76143
alesandro-ucb <alesandro.santander@ucb.edu.bo>
2025-08-27 15:38:36 -0400

    Declaracion de variables

---
+++ PRACTICA_01/Ejercicio_01_02.cpp
@@ -0,0 +1,34 @@
+
+// Materia: Programación I, Paralelo 4
+// Autor: Alesandro Raul Santander Fernandez
+// Carnet: 13552698 L.P.
+// Carrera del estudiante: Ingeniería en Telecomunicaciones
+// Fecha creación: 27/08/2025
+// Número de ejercicio: 2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int variable1 = 0;
+    int variable2 = 0;
+
+    cout << "Valores iniciales:" << endl;
+    cout << "variable1 = " << variable1 << endl;
+    cout << "variable2 = " << variable2 << endl;
+    //Muestra los valores los valores iniciales de las variables
+
+    cout << "Ingrese el valor de variable1: ";
+    cin >> variable1;
+    cout << "Ingrese el valor de variable2: ";
+    cin >> variable2;
+    // Muestra los valores ingresados por el usuario
+    cout << "Valores ingresados:" << endl;
+    cout << "variable1 = " << variable1 << endl;
+    cout << "variable2 = " << variable2 << endl;
+
+    return 0;
+
+}
+
+
