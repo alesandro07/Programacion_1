271fabe26a322b23e3afe947c34e666e24f7b99d
alesandro-ucb <alesandro.santander@ucb.edu.bo>
2025-08-27 18:47:24 -0400

    Update Ejercicio_01_07.cpp

--- PRACTICA_01/Ejercicio_01_07.cpp
+++ PRACTICA_01/Ejercicio_01_07.cpp
@@ -1,4 +1,9 @@
-
+// Materia: Programación I, Paralelo 4
+// Autor: Alesandro Raul Santander Fernandez
+// Carnet: 13552698 L.P.
+// Carrera del estudiante: Ingeniería en Telecomunicaciones
+// Fecha creación: 27/08/2025
+// Número de ejercicio: 7
 #include <iostream>
 using namespace std;
 int main(){
@@ -19,4 +24,5 @@ int main(){
         
             
 }
+
 
