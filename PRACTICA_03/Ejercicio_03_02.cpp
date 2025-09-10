ffd690e18431c02b887de0694d14dc34145560d5
alesandro-ucb <alesandro.santander@ucb.edu.bo>
2025-09-10 16:13:09 -0400

    formulas de grados

---
+++ PRACTICA_03/Ejercicio_03_02.cpp
@@ -0,0 +1,22 @@
+// Materia: Programación I, Paralelo 4 
+// Autor: Alesandro Raul Santander Fernández
+// Fecha creación: 10/09/2025 
+// Número de ejercicio: 1
+#include <iostream>
+using namespace std;
+
+// Función para convertir Celsius a Fahrenheit
+float celsiusAFahrenheit(float celsius) {
+    return (celsius * 9 / 5) + 32; // Formula de conversión
+}
+
+int main() {
+    float celsius;
+    cout << "Ingrese la temperatura en grados Celsius: ";
+    cin >> celsius;
+
+    float fahrenheit = celsiusAFahrenheit(celsius);
+    cout << celsius << " C equivalen a " << fahrenheit << " F" << endl;
+
+    return 0;
+}
