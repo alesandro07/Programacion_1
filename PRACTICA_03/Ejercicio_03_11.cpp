0238fd0742cadc2709b7cf100a0bf662f099dd37
alesandro-ucb <alesandro.santander@ucb.edu.bo>
2025-09-10 21:00:51 -0400

    cajero automatico

---
+++ PRACTICA_03/Ejercicio_03_11.cpp
@@ -0,0 +1,37 @@
+// Materia: Programación I, Paralelo 4 
+// Autor: Alesandro Raul Santander Fernández
+// Fecha creación: 10/09/2025 
+// Número de ejercicio: 11
+#include <iostream>
+using namespace std;
+
+// Función para retirar dinero
+double retirarDinero(double saldo, double cantidad) {
+    if (cantidad > saldo) {
+        cout << "Error: Saldo insuficiente." << endl;
+    }
+
+    if (int(cantidad) % 10 != 0) {
+        cout << "Error: El monto a retirar debe ser multiplo de 10." << endl;
+    }
+
+    if (cantidad <= saldo && int(cantidad) % 10 == 0) { // Retiro exitoso
+        saldo -= cantidad;
+        cout << "Retiro exitoso. Nuevo saldo: " << saldo << " Bs" << endl;
+    }
+
+    return saldo;
+}
+
+int main() {
+    double saldo = 1000.0;
+    double monto;
+
+    cout << "Ingrese el monto a retirar: ";
+    cin >> monto;
+
+    saldo = retirarDinero(saldo, monto); // Llamamos a la funcion
+
+    return 0;
+}
+
